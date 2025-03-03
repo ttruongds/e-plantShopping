cdfd31acc8e37e2ccfe26fdf0bbe256007e17111
ttruong <quynhthuyen@gmail.com>
2025-03-03 22:04:34 +1030

    edit checkout button

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -61,7 +61,7 @@ const CartItem = ({ onContinueShopping }) => {
       </div>
       <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'></div>
       <div className="continue_shopping_btn">
-        <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
+        <button className="get-started-button1" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
       </div>
       <div className="checkout_btn">
         <button className="checkout-button" onClick={handleCheckoutShopping}>Checkout</button>
