98099febcfddd74f7b0641e5d04efd7016819407
ttruong <quynhthuyen@gmail.com>
2025-03-09 16:52:13 +1030

    fix size

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
         <div className="background-image"></div>
         <div className="content">
-         <div className="landing_content">
+         <div class="container">
          <h1>Welcome To ABC plants!</h1>
           <div className="divider"></div>
           <p>Where Green Meets Serenity</p>
