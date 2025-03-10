8bbbb6a0885ba519c72d6256b7a094270ea891b5
ttruong <quynhthuyen@gmail.com>
2025-03-10 13:35:32 +1030

    fix twitter boostrap

--- src/App.jsx
+++ src/App.jsx
@@ -19,11 +19,10 @@ function App() {
       <div class="jumbotron">
         <div class="row">
           <div class="col-md-6">
-            <h1 className="text-center">Welcome To ABC plants!</h1>
-            
-            <p className="text-center">
-              Where Green Meets Serenity
-            </p>
+              <h1 className="text-center">Welcome To ABC plants!</h1>
+              <p className="text-center">
+                Where Green Meets Serenity
+              </p>
             <div className="d-inline-flex gap-2 mb-5">
               <button className="d-inline-flex align-items-center btn btn-primary btn-lg px-4 rounded-pill" onClick={handleGetStartedClick}>
                 Get Started
@@ -31,7 +30,6 @@ function App() {
             </div>
           </div>
           <div class="col-md-6">
-            <div>
               <p className="text-center">
               At ABC Plants, we are passionate about bringing nature closer to you. 
               Our mission is to provide a wide range of high-quality plants that 
@@ -43,7 +41,6 @@ function App() {
               Visit ABC plants shop today and experience the 
               beauty of nature right at your doorstep.
             </p>
-            </div>
           </div>
         </div>
       </div>
