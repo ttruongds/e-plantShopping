a3c1285cfe75b59275b0f2e76d7c58c9436eb061
ttruong <quynhthuyen@gmail.com>
2025-03-10 14:01:22 +1030

    fix twitter boostrap

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,6 @@ function App() {
   return (
     <div className="container my-5">
       <div class="jumbotron ${showProductList ? 'fade-out' : ''}">
-        <div className="background-image"></div>
         <div class="row">
           <div class="col-lg-8 mb-md-0 mb-3">
               <h1 className="text-center">Welcome To ABC plants!</h1>
@@ -30,6 +29,7 @@ function App() {
               </button>
             </div>
           </div>
+          <div className="divider"></div>
           <div class="col-lg-8 mb-md-0 mb-3">
               <p className="text-center">
               At ABC Plants, we are passionate about bringing nature closer to you. 
